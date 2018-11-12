001f1576a52cace3dc4c01379dd57475f93611c8
GILABERT OLTRA <chgiol@EVIRL-010-OK.dsic.cloud>
2018-11-12 17:00:04 +0100

    Initial commit

---
+++ dm_remote.js
@@ -0,0 +1,128 @@
+var net = require('net');
+
+var client = new net.Socket();
+
+exports.Start = function (host, port, cb) {
+	client.connect(port, host, function() {
+    	console.log('Connected to: ' + host + ':' + port);
+    	if (cb != null) cb();
+	});
+}
+
+
+var callbacks = {} // hash of callbacks. Key is invoId
+var invoCounter = 0; // current invocation number is key to access "callbacks".
+
+var MESSAGE_END = '#@FIN_MENSAJE@#';
+
+//
+// When data comes from server. It is a reply from our previous request
+// extract the reply, find the callback, and call it.
+// Its useful to study "exports" functions before studying this one.
+//
+client.on ('data', function (data) {
+	console.log ('data comes in: ' + data);
+	var reply = JSON.parse (data.toString());
+	switch (reply.what) {
+		// TODO complete list of commands
+		case 'get private message list':
+		case 'get public message list':
+		case 'get subject list':
+		case 'get user list':
+		case 'login':
+			console.log ('We received a reply for: ' + reply.what + ':' + reply.invoId);
+			callbacks [reply.invoId] (reply.obj); // call the stored callback, one argument
+			delete callbacks [reply.invoId]; // remove from hash
+			break;
+		case 'add private message':
+		case 'add public message':
+		case 'add subject':
+		case 'add user':
+			console.log ('We received a reply for add command');
+			callbacks [reply.invoId] (); // call the stored callback, no arguments
+			delete callbacks [reply.invoId]; // remove from hash
+			break;
+		default:
+			console.log ("Panic: we got this: " + reply.what);
+	}
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
+
+
+//
+// on each invocation we store the command to execute (what) and the invocation Id (invoId)
+// InvoId is used to execute the proper callback when reply comes back.
+//
+function Invo (str, cb) {
+	this.what = str;
+	this.invoId = ++invoCounter;
+	callbacks[invoCounter] = cb;
+}
+
+//
+//
+// Exported functions as 'dm'
+//
+//
+
+exports.getPublicMessageList = function  (sbj, cb) {
+	var invo = new Invo ('get public message list', cb);	
+	invo.sbj = sbj;
+	client.write (JSON.stringify(invo) + MESSAGE_END);
+}
+// Version del local
+
+// exports.getPrivateMessageList = function (u1, u2, cb) {
+// 	var list = dm.getPrivateMessageList (u1,u2);
+// 	cb (list);
+// }
+
+exports.getPrivateMessageList = function (u1, u2, cb) {
+	invo = new Invo ('get private message list', cb);
+	invo.u1 = u1;
+	invo.u2 = u2;
+	client.write (JSON.stringify(invo) + MESSAGE_END);
+}
+
+exports.addPrivateMessage = function (msg, cb){
+  
+	invo = new Invo ('add private message', cb);
+	invo.msg = msg;
+	client.write(JSON.stringify(invo) + MESSAGE_END);
+}
+
+exports.getSubjectList = function (cb) {
+	client.write (JSON.stringify(new Invo ('get subject list', cb)) + MESSAGE_END);
+}
+
+exports.addSubject = function (sbj, cb) {
+      invo = new Invo ('add subject', cb);
+      invo.sbj = sbj;
+      client.write(JSON.stringify(invo) + MESSAGE_END);
+}
+exports.addUser = function (u, p, cb){
+	invo= new Invo('add user', cb);
+	invo.u = u;
+	invo.p = p;
+	client.write(JSON.stringify(invo) + MESSAGE_END);
+}
+
+exports.getUserList = function (cb){
+	client.write (JSON.stringify(new Invo ('get user list', cb)) + MESSAGE_END);
+}
+
+exports.login = function (u, p, cb){
+	invo = new Invo('login', cb);
+	invo.u = u;
+	invo.p = p;
+	client.write(JSON.stringify(invo) + MESSAGE_END);
+}
+
+// DONE: complete the rest of the forum functions.
+
+
+
---
+++ dmclient.js
@@ -0,0 +1,107 @@
+var dm = require('./dm_remote.js');
+
+var HOST = '127.0.0.1';
+var PORT = 9001;
+var CMD = '';
+var CMD_ARG_1 = '';
+var CMD_ARG_2 = '';
+
+
+var args = process.argv.slice(2);
+if (args.length > 2) {
+	HOST = args[0];
+	PORT = args[1];
+
+	var commandParts = args.slice(2);
+	if (commandParts.length >= 1) {
+		CMD = commandParts[0];
+	}
+
+	if (commandParts.length > 1) {
+		CMD_ARG_1 = commandParts[1];
+	}
+	if (commandParts.length > 2) {
+		CMD_ARG_2 = commandParts[2];
+	}
+
+} else {
+	console.log('Usage: node dmclient.js HOST PORT CMD');
+	return;
+}
+
+function parseCommand(cmd, cb) {
+	switch (cmd) {
+		case 'get subject list':
+			dm.getSubjectList(function (ml) {
+				console.log("here it is:")
+				console.log(JSON.stringify(ml));
+				cb();
+			});
+			break;
+		case 'get public message list':
+			dm.getPublicMessageList(CMD_ARG_1, function (ml) {
+				console.log("here it is:")
+				console.log(JSON.stringify(ml));
+				cb();
+			});
+			break;
+		case 'get private message list':
+			dm.getPrivateMessageList(CMD_ARG_1, CMD_ARG_2, function (ml) {
+				console.log("here it is:")
+				console.log(JSON.stringify(ml));
+				cb();
+			});
+		case 'add subject':
+			dm.addSubject(CMD_ARG_1, function () {
+				console.log('subject ' + CMD_ARG_1 + ' added');
+				cb();
+			});
+			break;
+		case 'add user':
+			dm.addUser(CMD_ARG_1, CMD_ARG_2, function () {
+				console.log('user ' + CMD_ARG_1 + ' added');
+				cb();
+			});
+			break;
+		case 'add public message':
+			dm.addPublicMessage(CMD_ARG_1, function () {
+				console.log('public message ' + CMD_ARG_1 + ' added');
+				cb();
+			});
+			break;
+		case 'add private message':
+			dm.addPrivateMessage(CMD_ARG_1, function () {
+				console.log('public message ' + CMD_ARG_1 + ' added');
+				cb();
+			});
+			break;
+		case 'get user list':
+			dm.getUserList(function (ml) {
+				console.log("here it is:")
+				console.log(JSON.stringify(ml));
+				cb();
+			});
+			break;
+		case 'login':
+			dm.login(CMD_ARG_1, CMD_ARG_2, function (ml) {
+				console.log('user login: ' + JSON.stringify(ml))
+				cb();
+			});
+			break;
+		default:
+			dm.getSubjectList(function (ml) {
+				console.log("here it is:")
+				console.log(JSON.stringify(ml));
+				cb();
+			});
+			break;
+		// DONE: complete all forum functions
+	}
+}
+
+dm.Start(HOST, PORT, function () {
+	// Write the command to the server
+	parseCommand(CMD, function () {
+		console.log('command executed');
+	});
+});
