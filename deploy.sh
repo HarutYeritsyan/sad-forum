31010e4bd6d659deda1c84e095533f1b00c08178
YERITSYAN <haye@EVIRL-023-OK.dsic.cloud>
2019-01-26 21:19:29 +0100

    Added message list consulting to test2

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-npm run dmserver_rr-1 &
-npm run dmserver_rr-2 &
-npm run dmserver_rr-3 &
-npm run forum_rr-1 &
-npm run forum_rr-2 &
-npm run forum_rr-3 &
--- test2.sh
+++ test2.sh
@@ -1,2 +1,3 @@
 npm run dmclient_rr-1 'add public message' 'prueba client 1 S1' id0
+npm run dmclient_rr-2 'get public message list' id0
 npm run dmclient_rr-2 'add public message' 'prueba client 2 S2' id0
