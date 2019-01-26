f6e054992dd02d26844354a914457d9d194dc1ae
YERITSYAN <haye@EVIRL-023-OK.dsic.cloud>
2019-01-26 21:17:01 +0100

    Added Test2

--- test2.sh
+++ test2.sh
@@ -1,2 +1,2 @@
-npm run dmclient_rr-1 'add public message' 'prueba client 1' id0 2
-npm run dmclient_rr-2 'add public message' 'prueba client 2' id0 2
+npm run dmclient_rr-1 'add public message' 'prueba client 1 S1' id0
+npm run dmclient_rr-2 'add public message' 'prueba client 2 S2' id0
